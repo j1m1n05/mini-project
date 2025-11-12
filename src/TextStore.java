715d1f92999689a21fcb215bdf65fab215b9bd14
j1m1n05 <jstop172@gmail.com>
2025-11-12 22:17:53 +0900

    문자 목록 파일 입출력으로 수정

--- src/TextStore.java
+++ src/TextStore.java
@@ -1,13 +1,30 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class TextStore {
     private Vector<String> v=new Vector<String>();
     public TextStore(){  //파일 읽어서 등록하기
-        v.add("book");
-        v.add("love");
-        v.add("apple");
-        v.add("banana");
-        v.add("java");
+        FileReader file;
+        try{
+            file=new FileReader("src\\Text.txt");
+            StringBuffer sb=new StringBuffer();  //빈 스트링 버퍼
+            Scanner scanner=new Scanner(file);
+            while(scanner.hasNext()){
+                String word=scanner.nextLine();
+                word=word.trim();
+                v.add(word);
+            }
+            file.close();
+        } catch(FileNotFoundException e){
+            System.out.println("파일 열수 없음");
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("입출력 오류");
+            System.exit(2);
+        }
     }
     public String get(){
         int index=(int)(Math.random()*v.size());
